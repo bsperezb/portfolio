126402f490986333f32060235fa9fef7f549f749
Brayan Perez <brayanperezbuitrago@gmail.com>
2024-10-25 01:12:41 -0500

    Init navbar and banner description

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				primary: '#e4d9ff', 
+        secondary: '#1e2749', 
+        tertiary: '#273469', 
+        white: '#fafaff', 
+        dark: '#30343f', 
+			},
+			fontFamily: {
+        sans: ['Onest Variable', ...defaultTheme.fontFamily.sans],
+      },
+		},
+	},
+	plugins: [],
+}
