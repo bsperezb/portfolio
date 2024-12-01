a9be041ccf8d4bba0f6c04237dd84192cef93b75
Brayan Perez <brayanperezbuitrago@gmail.com>
2024-12-01 17:49:49 -0500

    Installed mdx and fixed markdown content render in workexperience

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [mdx(), tailwind()],
   output: 'static',
   adapter: vercel(),
 });
